2a668052787bb5cf5731d1f7be02c5d945b6f0a5
Abdelrahman Yousef Elhossiny Abounida <abdelrahman.abounida@tuhh.de>
2023-03-03 21:19:46 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useMode, ColorModeContext } from './assets/theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Navbar, CustomSidebar } from './pages/globals';
+import { Routes,Route } from 'react-router-dom';
+import {Default, Home, Profile,Calender,Excel} from './pages'
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import {Box} from '@mui/material';
 function App() {
+  const [theme, colorMode] = useMode()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                    
+                        <ProSidebarProvider>
+                          
+                        <Box className="App" >
+                          
+                          <CustomSidebar />
+                          <Box sx={{width:"80%"}}>
+                              <Navbar />
+                              <Routes>
+                                  <Route path="/" element={<Home />} />
+                                  <Route path="/default" element={<Default />}/>
+                                  <Route path="/calender" element={<Calender />}/>
+                                  <Route path="/excel" element={<Excel />}/>
+                                  <Route path="/profile" element={<Profile />}/>
+                                  <Route path="/logout" element={<Home />}/>
+                              </Routes>
+                          </Box>
+                        </Box>
+                        </ProSidebarProvider>
+                    </ThemeProvider>
+        </ColorModeContext.Provider>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export {default as Home} from './home/Home'
+export {default as Profile} from './profile/Profile'
+export {default as CustomSidebar} from './globals/CustomSidebar'
+export {default as Navbar} from './globals/Navbar'
+export {default as Calender} from './calender/Calender'
+export {default as Default} from './default/Default'
+export {default as Excel} from './excel/Excel'
+
+
